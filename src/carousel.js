28c8dedc61a1559cdc2575a8e4955c0c172e403c
Paula Pintado <85115054+Pauladriana@users.noreply.github.com>
2021-11-04 23:44:37 -0500

    Fix carousel images order

--- src/carousel.js
+++ src/carousel.js
@@ -26,8 +26,8 @@ const text1_options = [
     "https://firebasestorage.googleapis.com/v0/b/portafolio-bca12.appspot.com/o/project1.png?alt=media&token=1155846f-4d73-407d-a0fb-89cdf570e009",
     "https://firebasestorage.googleapis.com/v0/b/portafolio-bca12.appspot.com/o/project2.png?alt=media&token=7fe07df4-6255-46fb-ad39-db15e2ae217e",
     "https://firebasestorage.googleapis.com/v0/b/portafolio-bca12.appspot.com/o/project3.png?alt=media&token=b3d1f9dc-f551-4bba-bddf-cb1fbf824c2c",
-    "https://firebasestorage.googleapis.com/v0/b/portafolio-bca12.appspot.com/o/project4.png?alt=media&token=b44b0a93-8a20-4fa4-a38b-4e42a8324b91",
     "https://firebasestorage.googleapis.com/v0/b/portafolio-bca12.appspot.com/o/project5.png?alt=media&token=2f61c266-d3a7-44e2-a5b2-837ad0163e1a",
+    "https://firebasestorage.googleapis.com/v0/b/portafolio-bca12.appspot.com/o/project4.png?alt=media&token=b44b0a93-8a20-4fa4-a38b-4e42a8324b91",
     "https://firebasestorage.googleapis.com/v0/b/portafolio-bca12.appspot.com/o/project6.png?alt=media&token=e427220b-553b-4a7f-b0d8-e52c91056376"
   ];
   let i = 0;
